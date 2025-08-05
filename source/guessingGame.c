dfe4391a90047a671682fa2769c7dff708c47bdd
Ricardo Kromer Cavati <ricardokcavati@gmail.com>
2025-08-04 22:07:36 -0300

    Let user choose level of difficulty.

--- source/guessingGame.c
+++ source/guessingGame.c
@@ -8,19 +8,42 @@ int main()
     printf("* Welcome to our guessing game! *\n");
     printf("*********************************\n");
 
+    int difficultyLevel;
+    int maximumAttemptCount = 5;
+    printf("What is the difficulty level you would like to play on?\n");
+    printf("1 (easy).\n");
+    printf("2 (medium).\n");
+    printf("3 (hard).\n");
+    printf("Choose: ");
+    scanf("%d", &difficultyLevel);
+
+    switch (difficultyLevel)
+    {
+    case 1:
+        maximumAttemptCount = 15;
+        break;
+    case 2:
+        maximumAttemptCount = 10;
+        break;
+    default:
+        maximumAttemptCount = 5;
+        break;
+    }
 
     int userGuess = 0;
     int attempt = 1;
+
     float points = 1000;
-    
+    int guessedCorrectly = 0;
+
     int epochTimestampSeed = time(0);
     srand(epochTimestampSeed);
     int randomNumber = rand();
     int secretNumber = randomNumber % 100;
 
-    while (1)
+    for (int i = 1; i <= maximumAttemptCount; i++)
     {
-        printf("Attempt %d\n", attempt);
+        printf("Attempt %d of %d\n", attempt, maximumAttemptCount);
         printf("Please enter your guess: ");
         scanf("%d", &userGuess);
 
@@ -32,7 +55,7 @@ int main()
 
         printf("Your guess was %d!\n\n", userGuess);
 
-        int guessedCorrectly = secretNumber == userGuess;
+        guessedCorrectly = secretNumber == userGuess;
 
         if (guessedCorrectly)
         {
@@ -55,9 +78,16 @@ int main()
         printf("I'm sorry, you missed. Your guess is bigger than the secret number!\n");
     }
 
-    printf("Congratulations, you guessed it right on attempt number %d!\n", attempt);
-    printf("Total points: %.1f!\n", points);
     printf("Game over.\n");
+
+    if (guessedCorrectly)
+    {
+        printf("Congratulations, you guessed it right on attempt number %d!\n", attempt);
+        printf("Total points: %.1f!\n", points);
+        return 0;
+    }
+
+    printf("You lost. try again!\nThe secret number was %d\n", secretNumber);
 
     return 0;
 }
