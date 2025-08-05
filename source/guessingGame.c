1722b4b76f6c15d64d8bed7f1b8e906b73d9d757
Ricardo Kromer Cavati <ricardokcavati@gmail.com>
2025-08-04 21:03:30 -0300

    Let user try as many times as possible.

--- source/guessingGame.c
+++ source/guessingGame.c
@@ -1,43 +1,48 @@
 #include <stdio.h>
 
-#define MAX_ATTEMPT_NUMBER 5
-
 int main()
 {
-    int userGuess = 0;
-    int secretNumber = 42;
-
     printf("*********************************\n");
     printf("* Welcome to our guessing game! *\n");
     printf("*********************************\n");
 
-    for (int i = 1; i <= MAX_ATTEMPT_NUMBER; i++)
+    int userGuess = 0;
+    int secretNumber = 42;
+    int attempt = 1;
+
+    while (1)
     {
-        printf("Attempt %d of %d\n", i, MAX_ATTEMPT_NUMBER);
+        printf("Attempt %d\n", attempt);
         printf("Please enter your guess: ");
         scanf("%d", &userGuess);
 
-        printf("Your guess was %d!\n", userGuess);
-
-        int guessedRight = secretNumber == userGuess;
-
-        if (guessedRight)
+        if (userGuess < 0)
         {
-            printf("Congratulations, you guessed it right!\n");
+            printf("Your guess must be a positive number!\n\n");
+            continue;
+        }
+
+        printf("Your guess was %d!\n\n", userGuess);
+
+        int guessedCorrectly = secretNumber == userGuess;
+
+        if (guessedCorrectly)
+        {
+            printf("Congratulations, you guessed it right on attempt number %d!\n", attempt);
             break;
         }
 
-        printf("I'm sorry, you missed. ");
+        attempt++;
 
         int isSecretNumberBiggerThanGuess = secretNumber > userGuess;
 
         if (isSecretNumberBiggerThanGuess)
         {
-            printf("Your guess is smaller than the secret number!\n");
+            printf("I'm sorry, you missed. Your guess is smaller than the secret number!\n");
             continue;
         }
 
-        printf("Your guess is bigger than the secret number!\n");
+        printf("I'm sorry, you missed. Your guess is bigger than the secret number!\n");
     }
 
     printf("Game over.\n");
